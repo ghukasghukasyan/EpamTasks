a408c3bba31d02900ca48ff9ca0c8584cb472c26
Ghukas Ghukasyan <ghukas1997@mail.ru>
2022-09-01 07:02:22 +0400

    Interface And Abstraction

--- EpamTasks/Interface and Abstract Class/Airplane.cs
+++ EpamTasks/Interface and Abstract Class/Airplane.cs
@@ -6,15 +6,23 @@ namespace EpamTasks.Interface_and_Abstract_Class
 {
     class Airplane : IFlyable
     {
-        
-        int initialSpeed = 200;
-        Cordinates currentPosition { get; set; }
+
+        private readonly int initialSpeed = 200;
+        Cordinates CurrentPosition { get; set; }
 
         
         public void FlyTo(Cordinates newPosition)
         {
-            Console.WriteLine("The distance between this to points is");
-            var distance = Math.Sqrt(Math.Pow((newPosition.X - currentPosition.X), 2) + Math.Pow((newPosition.Y - currentPosition.Y), 2) + Math.Pow((newPosition.Z - currentPosition.Z), 2));
+            
+            var distance = Math.Sqrt(Math.Pow((newPosition.X - CurrentPosition.X), 2) + Math.Pow((newPosition.Y - CurrentPosition.Y), 2) + Math.Pow((newPosition.Z - CurrentPosition.Z), 2));
+            if (distance > 10000)
+            {
+                Console.WriteLine("Airplane couldn't fly more than 10,000km");
+            }
+            else
+            {
+                Console.WriteLine("The distance between this two points is : " + distance);
+            };
 
 
         }
@@ -22,10 +30,36 @@ namespace EpamTasks.Interface_and_Abstract_Class
 
         public void GetFlyTime(Cordinates newPosition)
         {
-            
+            var distance = Math.Sqrt(Math.Pow((newPosition.X - CurrentPosition.X), 2) + Math.Pow((newPosition.Y - CurrentPosition.Y), 2) + Math.Pow((newPosition.Z - CurrentPosition.Z), 2));
+            if (distance > 10000)
+            {
+                Console.WriteLine("Airplane couldn't fly more than 10,000km");
+            }
+            else
+            {
+                Console.WriteLine("The distance between this two points is : " + distance);
+
+            };
+            int currentSpeed = initialSpeed + (int)distance / 10;
+            var flyTime = distance / currentSpeed;
+            Console.WriteLine("The fly time is : " + flyTime);
+
         }
 
-       
+        public Airplane()
+        {
+
+        }
+
+        public Airplane(Cordinates currentPosition)
+        {
+            CurrentPosition = currentPosition;
+        }
+        public override string ToString()
+        {
+            return $"Airplane Cordinates are X = {CurrentPosition.X}, Y = {CurrentPosition.Y}, Z = {CurrentPosition.Z}";
+        }
+
 
     }
 }
